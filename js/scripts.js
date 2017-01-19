3c7f515438409e97fce12b3adb6d8a770d908322
Elise St Hilaire and keely glenn <keely@>
2017-01-19 11:55:19 -0800

    convert single digits to roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,24 @@ var convertToRomanNumerals = function(userInput) {
   var splitInput = userInput.split('');
   var isNumber = true;
 
+// single digits
+  var singleConvert = function(userInput) {
+    var singleDigit = parseInt(splitInput[splitInput.length -1]);
+    if (singleDigit === 1 || singleDigit === 2 || singleDigit === 3 ) {
+      return "I".repeat(singleDigit);
+    } else if (singleDigit === 4) {
+      return "IV";
+    } else if (singleDigit === 5) {
+      return "V";
+    } else if (singleDigit === 6 || singleDigit === 7 || singleDigit === 8) {
+      return "V" + "I".repeat(singleDigit-5);
+    } else if (singleDigit === 9) {
+      return "IX";
+    } else {
+      return "X";
+    }
+  }
+
 // determines if input is convertable
   splitInput.forEach(function(number) {
     var numQuery = numbers.indexOf(number);
@@ -12,13 +30,12 @@ var convertToRomanNumerals = function(userInput) {
       isNumber = false;
     }
   });
-  
+
   if (!isNumber) {
     return "Your input cannot be converted. Check input";
   } else {
-    return "Your number is " + userInput;
+    return "Your number is " + singleConvert(userInput);
   }
-
 
 }
 
