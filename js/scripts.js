27215982ec4d5e58d5bc77dda791d0d80aec865e
Elise St Hilaire and keely glenn <keely@>
2017-01-19 15:34:15 -0800

    refactor single and double digits into 1 loop function. Includes 100s, and 1000s.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,33 @@
 //back end
 
 var convertToRomanNumerals = function(userInput) {
+  var tens = ['I', 'X', 'C', 'M'];
+  var fives = ["V", "L", "D", "filler"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var splitInput = userInput.split('');
   var isNumber = true;
+  var splitInput = userInput.split("");
+  // var digit = parseInt(splitReverseInput[splitInput.length -1]);
 
-// single digits
-  var singleConvert = function(userInput) {
-    var singleDigit = parseInt(splitInput[splitInput.length -1]);
-    if (singleDigit === 1 || singleDigit === 2 || singleDigit === 3 ) {
-      return "I".repeat(singleDigit);
-    } else if (singleDigit === 4) {
-      return "IV";
-    } else if (singleDigit === 5) {
-      return "V";
-    } else if (singleDigit === 6 || singleDigit === 7 || singleDigit === 8) {
-      return "V" + "I".repeat(singleDigit-5);
-    } else if (singleDigit === 9) {
-      return "IX";
-    } else {
-      return "";
+// all digit
+var multiConvert = function(userInput) {
+  var splitReverseInput = splitInput.reverse();
+  var answer = [];
+  for (i = 0; i < splitReverseInput.length; i++) {
+    if (splitReverseInput[i] === "1" || splitReverseInput[i] === "2" || splitReverseInput[i] === "3") {
+      answer.unshift(tens[i].repeat(splitReverseInput[i]));
+    }  else if (splitReverseInput[i] === "4") {
+      answer.unshift(tens[i] + fives[i]);
+    } else if (splitReverseInput[i] === "5") {
+      answer.unshift(fives[i]);
+    } else if (splitReverseInput[i] === "6" || splitReverseInput[i] === "7" || splitReverseInput[i] === "8") {
+      answer.unshift(fives[i] + (tens[i].repeat(parseInt(splitReverseInput[i])-5)));
+    } else if (splitReverseInput[i] === "9") {
+      answer.unshift(tens[i] + tens[i+1]);
     }
   }
-
-  var doubleConvert = function(userInput) {
-    var doubleDigit = parseInt(splitInput[splitInput.length -2]);
-    if (doubleDigit === 1 || doubleDigit === 2 || doubleDigit === 3 ) {
-      return "X".repeat(doubleDigit);
-    } else if (doubleDigit === 4) {
-      return "IL";
-    } else if (doubleDigit === 5) {
-      return "L";
-    } else if (doubleDigit === 6 || doubleDigit === 7 || doubleDigit === 8) {
-      return "L" + "X".repeat(doubleDigit-5);
-    } else if (doubleDigit === 9) {
-      return "XC";
-    } else {
-      return "";
-    }
+  return answer.join('');
 }
+
 // determines if input is convertable
   splitInput.forEach(function(number) {
     var numQuery = numbers.indexOf(number);
@@ -50,9 +39,8 @@ var convertToRomanNumerals = function(userInput) {
   if (!isNumber) {
     return "Your input cannot be converted. Check input";
   } else {
-    return "Your number is " + doubleConvert(userInput) + singleConvert(userInput);
+    return "Your number is " + multiConvert(userInput);
   }
-
 }
 
 
@@ -70,3 +58,47 @@ $(document).ready(function() {
     $("#result").text(result);
   });
 });
+
+
+
+
+
+
+
+
+
+
+// // single
+//   var singleConvert = function(splitInput) {
+//     var singleDigit = parseInt(splitInput[splitInput.length -1]);
+//     if (singleDigit === 1 || singleDigit === 2 || singleDigit === 3 ) {
+//       return "X".repeat(singleDigit);
+//     } else if (singleDigit === 4) {
+//       return "XL";
+//     } else if (singleDigit === 5) {
+//       return "L";
+//     } else if (singleDigit === 6 || singleDigit === 7 || singleDigit === 8) {
+//       return "v" + "I".repeat(singleDigit-5);
+//     } else if (singleDigit === 9) {
+//       return "XC";
+//     } else {
+//       return "";
+//     }
+//   }
+// // double
+//   var doubleConvert = function(splitInput) {
+//     var doubleDigit = parseInt(splitInput[splitInput.length -2]);
+//     if (doubleDigit === 1 || doubleDigit === 2 || doubleDigit === 3 ) {
+//       return "X".repeat(doubleDigit);
+//     } else if (doubleDigit === 4) {
+//       return "XL";
+//     } else if (doubleDigit === 5) {
+//       return "L";
+//     } else if (doubleDigit === 6 || doubleDigit === 7 || doubleDigit === 8) {
+//       return "L" + "X".repeat(doubleDigit-5);
+//     } else if (doubleDigit === 9) {
+//       return "XC";
+//     } else {
+//       return "";
+//     }
+// }
